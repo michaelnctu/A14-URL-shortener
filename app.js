ad4d4af107b1d7c4743fcb8346e80b3bdc12c442
MIchael Huang <michaelnctu@gmail.com>
2020-08-15 16:54:05 +0800

    產生隨機url

--- app.js
+++ app.js
@@ -4,11 +4,29 @@ const PORT = process.env.PORT || 3000
 
 const mongoose = require('mongoose') // 載入 mongoose
 
+const exphbs = require('express-handlebars')
+
+
 mongoose.connect('mongodb://localhost/shorten-url') // 設定連線到 mongoDB
 
+const bodyParser = require('body-parser')
+const routes = require('./routes')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//default handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
+
+app.post('/', (req, res) => {
+  console.log('req', req.body)
+})
+
 
 // start and listen on the Express server
 app.listen(PORT, () => {
---
+++ routes/modules/randomURL.js
@@ -0,0 +1,33 @@
+
+//產生密碼
+function generateURL() {
+  // define things user might want
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseLetters = lowerCaseLetters.toUpperCase()
+  const numbers = '1234567890'
+
+  let collection = []
+
+  collection = collection.concat(lowerCaseLetters.split(''))  //小寫
+  collection = collection.concat(upperCaseLetters.split('')) //大寫
+  collection = collection.concat(numbers.split('')) // number放入 陣列如此 [a, n, c] 皆為string
+
+  let password = ''
+  for (let i = 0; i < 5; i++) {
+    password += sample(collection)
+
+  }
+
+  // return the generated password
+  return password
+
+}
+
+// define sample function to randomly return a item in an array
+function sample(array) {
+  const index = Math.floor(Math.random() * array.length)
+  return array[index]
+}
+
+// export generatePassword function for other files to use
+module.exports = generateURL
